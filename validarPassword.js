349fbcacf6dc0bfce359d76a1bf6764f43d87cad
Alvaro <alv.canete@duocuc.cl>
2024-03-17 23:58:55 -0300

    responsive toda la pagina

--- validarPassword.js
+++ validarPassword.js
@@ -1,16 +1,21 @@
 var myInput = document.getElementById("pass1");
 
-myInput.onkeyup = function() {
+myInput.onkeyup = function () {
     var upperCaseLetters = /[A-Z]/g;
-  if(myInput.value.match(upperCaseLetters)) {
-    var numbers = /[0-9]/g;
-    if(myInput.value.match(numbers)){
-        if(myInput.value.length >= 8) {
-            if(myInput.value.length <= 16) {
-                myInput.classList.remove("is-invalid");
-                myInput.classList.add("is-valid");
-                document.getElementById("Enviar").disabled = false;
-            } else{
+    if (myInput.value.match(upperCaseLetters)) {
+        var numbers = /[0-9]/g;
+        if (myInput.value.match(numbers)) {
+            if (myInput.value.length >= 8) {
+                if (myInput.value.length <= 16) {
+                    myInput.classList.remove("is-invalid");
+                    myInput.classList.add("is-valid");
+                    document.getElementById("Enviar").disabled = false;
+                } else {
+                    myInput.classList.remove("is-valid");
+                    myInput.classList.add("is-invalid");
+                    document.getElementById("Enviar").disabled = true;
+                }
+            } else {
                 myInput.classList.remove("is-valid");
                 myInput.classList.add("is-invalid");
                 document.getElementById("Enviar").disabled = true;
@@ -25,18 +30,13 @@ myInput.onkeyup = function() {
         myInput.classList.add("is-invalid");
         document.getElementById("Enviar").disabled = true;
     }
-  } else {
-    myInput.classList.remove("is-valid");
-    myInput.classList.add("is-invalid");
-    document.getElementById("Enviar").disabled = true;
-  }
 }
 
 var passMatch = document.getElementById("pass2");
 
-passMatch.onkeyup = function() {
+passMatch.onkeyup = function () {
     var myInput = document.getElementById("pass1");
-    if(myInput.value === passMatch.value){
+    if (myInput.value === passMatch.value) {
         passMatch.classList.remove("is-invalid");
         passMatch.classList.add("is-valid");
         document.getElementById("Enviar").disabled = false;
